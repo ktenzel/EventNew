ef17c0daf86c4fca6bb761afb92c9fb9d09962fc
kyle tenzeldam <ktenzel@gmail.com>
2018-01-08 01:23:27 -0800

    All tests pass and app runs

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -19,26 +19,42 @@ public class EventTest {
 
     @Test
     public void getFood() {
-
+        Event testEvent = new Event(20, "Chicken", "Soda", "DJ", "");
+        assertEquals("Chicken", testEvent.getFood());
     }
-//
-//    @Test
-//    public void getDrink() {
-//    }
-//
-//    @Test
-//    public void getEntertainment() {
-//    }
-//
-//    @Test
-//    public void getCoupon() {
-//    }
-//
-//    @Test
-//    public void calculatePlate() {
-//    }
-//
-//    @Test
-//    public void totalCost() {
-//    }
+
+    @Test
+    public void getDrink() {
+        Event testEvent = new Event(20, "Chicken", "Soda", "DJ", "");
+        assertEquals("Soda", testEvent.getDrink());
+    }
+
+    @Test
+    public void getEntertainment() {
+        Event testEvent = new Event(20, "Chicken", "Soda", "DJ", "");
+        assertEquals("DJ", testEvent.getEntertainment());
+    }
+
+    @Test
+    public void getCoupon() {
+        Event testEvent = new Event(20, "Chicken", "Soda", "DJ", "");
+        assertEquals("", testEvent.getCoupon());
+    }
+
+    @Test
+    public void getPlate() {
+        Event testEvent = new Event(20, "Chicken", "Soda", "DJ", "");
+        assertEquals(0, testEvent.getPlate());
+    }
+    @Test
+    public void calculatePlate() {
+        Event testEvent = new Event(20, "Chicken", "Soda", "DJ", "");
+        assertEquals(32, testEvent.calculatePlate());
+    }
+
+    @Test
+    public void totalCost() {
+        Event testEvent = new Event(20, "Chicken", "Soda", "DJ", "");
+        assertEquals(790, testEvent.totalCost());
+    }
 }
